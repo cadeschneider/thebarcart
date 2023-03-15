0372b0560f02a6cb4ee614518e895ca9f86c406b
Cade Schneider <cadeschneider25@gmail.com>
2023-03-14 17:21:27 -0700

    revised background image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,9 @@ import '../App.css';
 function App() {
   return (
     <div className='homescreen'>
-      <img className='background' src='https://images.unsplash.com/photo-1525268323446-0505b6fe7778?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'></img>
+      <div className='background'></div>
     </div>
+    
     
   );
 }
