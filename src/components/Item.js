18bc1a1bf2b8d48f3fc0f62d78236acb241c2920
Cade Schneider <cadeschneider25@gmail.com>
2023-03-15 20:51:39 -0700

    changed stylesheet

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,8 +5,8 @@ function Item(props) {
     return(
 
         <div className="drink" onClick={props.handleClick}>
-            <h3 className="drink--name">{props.item}</h3>
             {props.image !== undefined && <img className="drink--image" src={props.image} />}
+            <h3 className="drink--name">{props.item}</h3>
         </div>
 
     )
